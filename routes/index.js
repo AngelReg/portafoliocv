d45e316ece58a187b9cafac06ca640f20e978020
Angel Ivan Regules Bautista <angeldreyar2@gmail.com>
2018-03-01 11:22:04 -0600

    subiendo el CV

---
+++ routes/index.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Angel Ivan Regules Bautista' });
+});
+
+module.exports = router;
